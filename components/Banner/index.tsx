cd369030c563d0b1edddf5ddf7dcd4ab632944a4
Joebert <jlsembrero@findme.com.ph>
2023-06-06 12:26:22 +0800

    mini adjustment in banner

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -14,7 +14,7 @@ const Banner = () => {
             <h1 className="font-bold text-3xl">
               Hi, I'm Joe Bert
             </h1>
-            <h3 className="font-medium text-gray-500">Frontend developer</h3>
+            <h3 className="font-medium text-gray-500 underline underline-offset-4 decoration-dotted">Frontend developer</h3>
             <p className="text-sm text-gray-400">
               Currently, I am working as frontend developer at WebCast Technologies Inc. 🐱‍🏍
             </p>
