57e25f02d96b473002368134633a6c68f792a25c
Joebert <jlsembrero@findme.com.ph>
2023-06-06 12:26:37 +0800

    adjustment in space

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="max-w-lg mx-auto space-y-12 md:max-w-2xl lg:max-w-3xl xs:px-4 space-y-4 sm:overflow-visible overflow-hidden">
+    <div className="max-w-lg mx-auto md:max-w-2xl lg:max-w-3xl xs:px-4 sm:overflow-visible overflow-hidden">
       <Banner />
       {/* <section className="relative rounded-2xl shadow-lg h-[120px] w-[90%] mx-auto bg-blue-500">
 
