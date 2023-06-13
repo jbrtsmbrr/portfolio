07e55efde05ec08388a8a3700dc19b731e452c40
Joebert <jlsembrero@findme.com.ph>
2023-06-13 22:42:31 +0800

    added: dark mode in hard skills

--- components/HardSkills/SkillCard.tsx
+++ components/HardSkills/SkillCard.tsx
@@ -9,13 +9,14 @@ const SkillCard = ({
 }: SkillCardProp) => {
   return (
     <div
-      className={`shadow-md w-full flex p-3 place-items-center gap-4 rounded-xl h-full bg-white ${learning ? "border border-slate-300 border-dashed" : ""
+      className={`shadow-md w-full flex p-3 place-items-center gap-4 rounded-xl h-full bg-white dark:bg-slate-800
+      transition duration-700 ${learning ? "border border-slate-400 border-dashed dark:border-slate-600" : ""
         }`}
     >
       <span style={{ background }} className="p-1 rounded-lg min-h-full flex">
         <Image src={imageUrl} alt={label} width={30} height={30} />
       </span>
-      <span className="text-sm font-medium text-gray-600">{label}</span>
+      <span className="text-sm font-medium text-gray-600 dark:text-gray-100 transition duration-700">{label}</span>
     </div>
   );
 };
