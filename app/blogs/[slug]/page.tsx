c06eca01384e893af55a7edcda44773099d6d0bb
Joebert <jlsembrero@findme.com.ph>
2023-06-13 23:12:13 +0800

    added: dark mode in blogs content

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -26,10 +26,10 @@ const BlogPost = (props: any) => {
   const blog = getBlogPost(slug);
   return (
     <div className="max-w-2xl mx-auto py-6 px-4">
-      <p className="text-sm text-gray-600 font-light">{blog.data.date}</p>
+      <p className="text-sm text-gray-600 font-light dark:text-gray-400">{blog.data.date}</p>
       <h1 className="text-3xl font-bold">{blog.data.title}</h1>
-      <p className="text-md text-gray-600">{blog.data.subtitle}</p>
-      <article className="prose prose-md">
+      <p className="text-md text-gray-600 dark:text-gray-400">{blog.data.subtitle}</p>
+      <article className="prose prose-md dark:text-gray-300 prose-headings:dark:text-gray-300 prose-blockquote:dark:text-gray-300 prose-strong:dark:text-gray-300 prose-code:dark:text-gray-300 prose-em:dark:text-gray-300 prose-a:dark:text-gray-300 prose-li:dark:text-gray-300 prose-ul:dark:text-gray-300 prose-ol:dark:text-gray-300">
         <Markdown>{blog.content}</Markdown>
       </article>
     </div>
