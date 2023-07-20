06911089d4b043b93b14d5670f122aea5c852783
Joebert <jlsembrero@findme.com.ph>
2023-07-20 23:31:49 +0800

    added: experimental serverActions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true // this experimental feature is for "use server" directives
+  }
+}
 
 module.exports = nextConfig
