a082d307d5207e8903b7eb4ce126344a56e7aed1
Joebert <jlsembrero@findme.com.ph>
2023-07-21 15:44:18 +0800

    remove: all loggers

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Contact from "@/components/Contact";
 import logger from "@/components/__utils/log";
 
 export default async function Home() {
-  await logger("PAGE_LOADED");
+  // await logger("PAGE_LOADED");
   return (
     <div className="max-w-lg mx-auto md:max-w-2xl lg:max-w-3xl xs:px-4 sm:overflow-visible overflow-hidden">
       <Banner />
