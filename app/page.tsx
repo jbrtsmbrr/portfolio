b79c699d31f7793b9435ac68899fa235d8860d4a
Joebert <jlsembrero@findme.com.ph>
2023-07-21 22:36:56 +0800

    remove: all logger direct to component

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,20 @@ import Image from "next/image";
 import Contact from "@/components/Contact";
 import logger from "@/components/__utils/log";
 
+const addLogs = async (module: string, description?: string) => {
+  const response = await fetch(`${process.env.BASE_URL}/logs`, {
+    method: "POST",
+    body: JSON.stringify({ module, description }),
+    cache: "no-store"
+  })
+
+  const result = await response.json();
+
+  return result;
+}
+
 export default async function Home() {
-  // await logger("PAGE_LOADED");
+  await addLogs("PAGE_LOADED");
   return (
     <div className="max-w-lg mx-auto md:max-w-2xl lg:max-w-3xl xs:px-4 sm:overflow-visible overflow-hidden">
       <Banner />
