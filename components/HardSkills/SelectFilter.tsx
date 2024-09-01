4ae68f441b5ad210794d5ac040bf9246acb0cd18
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-09-01 15:36:53 +0800

    fix: strokewidth

--- components/HardSkills/SelectFilter.tsx
+++ components/HardSkills/SelectFilter.tsx
@@ -34,7 +34,7 @@ const SelectFilter = ({ value, onSelectedChanged }: TSelectedFilter) => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            stroke-width="2"
+            strokeWidth="2"
             d="M19 9l-7 7-7-7"
           ></path>
         </svg>
