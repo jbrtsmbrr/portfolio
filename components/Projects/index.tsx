6e204b3314568d3b0623b9487bb78b28a791d952
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-09-04 23:45:39 +0800

    apply: ProjectInfo component in project section

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -56,6 +56,8 @@ const Projects = () => {
           </div>
         ))}
       </div>
+      {/* SHEET */}
+      <ProjectInfo project={selectedProject} onSelectedProjectChange={handleProjectSelected} />
     </section>
   );
 };
