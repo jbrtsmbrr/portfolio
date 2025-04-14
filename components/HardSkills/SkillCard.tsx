a0cfd48daa171e7dfaf1baad430937cadb5d7dcd
jbrtsmbrr <jbrt.dev@gmail.com>
2025-04-14 16:10:44 +0800

    refactor: adjust font weight and animation duration of SkillCard component

--- components/HardSkills/SkillCard.tsx
+++ components/HardSkills/SkillCard.tsx
@@ -13,10 +13,10 @@ const SkillCard = ({
       transition duration-700 ${learning ? "border border-slate-400 border-dashed dark:border-slate-600" : ""
         }`}
     >
-      <span style={{ background }} className="p-1 rounded-lg min-h-full flex shadow-md">
+      <span style={{ background }} className="flex min-h-full p-1 rounded-lg shadow-md">
         <Image src={imageUrl} alt={label} width={30} height={30} />
       </span>
-      <span className="text-sm font-medium text-gray-600 dark:text-gray-100 transition duration-700">{label}</span>
+      <span className="text-sm font-normal text-gray-600 transition duration-100 dark:text-gray-100">{label}</span>
     </div>
   );
 };
